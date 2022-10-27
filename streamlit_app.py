c69527ba3303bea1e662f66146845d44ae36f975
adjohans <116809869+adjohans@users.noreply.github.com>
2022-10-27 15:35:21 +0200

    Update streamlit_app.py
    Fixed an error on line 8 with no end parantheses

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.heder('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinache & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg'
+streamlit.text('Hard-Boiled Free-Range Egg')
