70627f93231108db93c36595aa7a0e63b920eb9b
adjohans <116809869+adjohans@users.noreply.github.com>
2022-11-04 13:14:37 +0100

    Update streamlit_app.py
    fixed typos, in the earlier added function

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_gruityvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_frunction) #output it on the screen as a table
 
 except URLError as e:
